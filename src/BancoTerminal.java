c04d491f19af640c5cdfcc59167a4652c9ae00fc
soulrafael <rafaelbsoul@gmail.com>
2024-05-23 23:50:07 -0300

    0.0

---
+++ src/BancoTerminal.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class BancoTerminal {
+    Scanner entrada = new Scanner(System.in);
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!Banco Terminal");
+        Login(null, null);
+
+
+
+
+
+
+
+    }
+    public static void Login(String usuario, String senha){
+
+    }
+}
---
+++ src/Usuario/Usuario.java
@@ -0,0 +1,9 @@
+package Usuario;
+
+public class Usuario {
+    public String nome;
+    public int cpf;
+    public int idade;
+
+    
+}
